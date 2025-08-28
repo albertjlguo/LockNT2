02634fa347b41b269b16fc56afbd3ebe66420cee
Albert Guo <sub.jl@icloud.com>
2025-08-28 11:05:31 +0000

    back to 修复目标检测框准确性问题

--- app.py
+++ app.py
@@ -15,5 +15,4 @@ app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 from routes import *
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5001))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
